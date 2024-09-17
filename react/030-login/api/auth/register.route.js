96f4110ee57b6942a7fbd8c20bc2fe02d4e1f221
fchristian1 <fchristian1@gmx.net>
2024-09-17 23:12:28 +0200

    20240917-231228

--- react/030-login/api/auth/register.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { registerController } from "./register.controller.js";
-
-const router = express.Router();
-
-router.post("/register", registerController);
-
-export { router as registerRouter };
--- react/030-login/api/users/user.router.js
+++
@@ -1,8 +0,0 @@
-import Router from "express";
-import { readAllUsersController } from "./user.controller.js";
-
-const router = Router();
-
-router.get("/", readAllUsersController);
-
-export { router as userRouter };
